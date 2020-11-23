917205ee9e6de96190d7d9e3ed021b78a534cd96
Naseef Mayanja <nmayanja@idi.co.ug>
2020-11-23 14:07:24 +0300

    added a print file with my bio

---
+++ naseef_biodata.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 23 13:56:36 2020
+
+@author: nmayanja
+"""
+
+fname = "naseef"
+lname = "mayanja"
+age = 19
+adress = "IDI, MKC"
+print (fname, lname,age,adress);
